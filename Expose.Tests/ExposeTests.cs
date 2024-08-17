d333acdcbcf2da9f9adee60d1df32874c909a658
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2024-08-17 10:48:34 +1000

    Add project files.

---
+++ Expose.Tests/ExposeTests.cs
@@ -0,0 +1,21 @@
+﻿using Expose;
+using FluentAssertions;
+using System;
+using System.Linq.Expressions;
+using Xunit;
+
+namespace Expose.Tests
+{
+    public class ExposeTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Expression<Func<int, string>> first = x => x.ToString();
+            var composed = first.Then(x => x + "!");
+            var input = 4;
+            var output = composed.Compile().Invoke(input);
+            output.Should().Be("4!");
+        }
+    }
+}
---
+++ Expose/Expose.cs
@@ -0,0 +1,24 @@
+﻿namespace Expose;
+
+using System.Linq.Expressions;
+
+public static class Expose
+{
+    /// <summary>
+    /// Returns an expression that is equivalent to <c>x => second(first(x))</c>
+    /// </summary>
+    public static Expression<Func<T1, T3>> Then<T1, T2, T3>(
+        this Expression<Func<T1, T2>> first,
+        Expression<Func<T2, T3>> second)
+    {
+        var firstParameter = first.Parameters[0];
+        var firstBody = first.Body;
+
+        var replacedBody = Expression.Invoke(second, firstBody);
+
+        return Expression.Lambda<Func<T1, T3>>(replacedBody, firstParameter);
+    }
+
+    public static Expression<?> Wrap<>(
+        )
+}
