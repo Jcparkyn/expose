c7101d87d0c160a6c0e4887aa9db8017d8f08a8b
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2024-08-17 12:22:16 +1000

    Misc

--- Expose.Tests/ExposeTests.cs
+++ Expose.Tests/ExposeTests.cs
@@ -2,6 +2,7 @@
 using FluentAssertions;
 using System;
 using System.Linq.Expressions;
+using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace Expose.Tests
@@ -9,13 +10,54 @@ namespace Expose.Tests
     public class ExposeTests
     {
         [Fact]
-        public void Test1()
+        public void Then()
         {
             Expression<Func<int, string>> first = x => x.ToString();
             var composed = first.Then(x => x + "!");
             var input = 4;
             var output = composed.Compile().Invoke(input);
             output.Should().Be("4!");
+        }
+
+        [Fact]
+        public void Wrap()
+        {
+            Expression<Func<int, int>> addOne = x => x + 1;
+            //var wrapped = Expose.Wrap<int, int, bool>(addOne, addOne =>
+            //    x => addOne(x) == 3
+            //);
+            var wrapped = Expose.Returning_<bool>().Wrap(addOne, addOne =>
+                x => addOne(x) == 3
+            );
+            var output1 = wrapped.Compile().Invoke(1);
+            output1.Should().BeFalse();
+            var output2 = wrapped.Compile().Invoke(2);
+            output2.Should().BeTrue();
+        }
+
+        [Fact]
+        public void Wrap2()
+        {
+            Expression<Func<int, bool>> isNegative = x => x < 0;
+            Expression<Func<int, int>> mod2 = x => x % 2;
+            var wrapped = Expose.Returning<Func<int, bool>>.Wrap(
+                (isNegative, mod2),
+                (isNegative, mod2) =>
+                    (int x) => isNegative(x) || mod2(x) == 1
+            );
+
+            var wrapped2 = Expose.Accepting<int>.Wrap(
+                (isNegative, mod2),
+                (isNegative, mod2) =>
+                    (Func<int, bool>)(x => isNegative(x) || mod2(x) == 1)
+            );
+
+            var wrapped3 = Expose.WrapSimple2(
+                (isNegative, mod2),
+                (isNegative, mod2) => Expose.Func(
+                    (int x) => isNegative(x) || mod2(x) == 1
+                )
+            );
         }
     }
 }
--- Expose/Expose.cs
+++ Expose/Expose.cs
@@ -19,6 +19,123 @@ public static class Expose
         return Expression.Lambda<Func<T1, T3>>(replacedBody, firstParameter);
     }
 
-    public static Expression<?> Wrap<>(
-        )
+    public static Expression<Func<T1, T3>> Wrap<T1, T2, T3>(
+        this Expression<Func<T1, T2>> wrapped,
+        Expression<Func<
+            Func<T1, T2>,
+            Expression<Func<T1, T3>>
+        >> wrapper)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Expression<Func<FRet1, FRet2>> WrapSimple<F1, FRet1, FRet2>(
+        this Expression<F1> wrapped,
+        Func<F1, Func<FRet1, FRet2>> wrapper)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Expression<Func<T1, bool>> WrapReturningBool<T1, T2>(
+        this Expression<Func<T1, T2>> wrapped,
+        Expression<Func<
+            Func<T1, T2>,
+            Expression<Func<T1, bool>>
+        >> wrapper)
+    {
+        throw new NotImplementedException();
+    }
+
+    //public static Expression<Func<TInput, bool>> Wrap<TInput, TOutput>(
+    //    Expression<Func<TInput, TOutput>> originalExpression,
+    //    Expression<Func<
+    //        Expression<Func<TInput, TOutput>>,
+    //        Expression<Func<TInput, bool>>
+    //    >> wrapper)
+    //{
+    //    throw new NotImplementedException();
+    //}
+
+    public static ExposeReturning<T> Returning_<T>() => new();
+
+    public static class Returning<TReturn>
+    {
+        public static Expression<TReturn> Wrap<F1, F2>(
+            (Expression<F1>, Expression<F2>) wrapped,
+            Func<F1, F2, TReturn> wrapper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static class Accepting<TIn>
+    {
+        public static Expression<Func<TIn, TOut>> Wrap<F1, F2, TOut>(
+            (Expression<F1>, Expression<F2>) wrapped,
+            Func<F1, F2, Func<TIn, TOut>> wrapper)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public static Expression<TReturn> WrapSimple2<F1, F2, TReturn>(
+        (Expression<F1>, Expression<F2>) wrapped,
+        Expression<Func<F1, F2, TReturn>> wrapper)
+    {
+        throw new NotImplementedException();
+    }
+
+    public static Func<T1, T2> Func<T1, T2>(Func<T1, T2> f) => f;
+}
+
+public record struct ExposeReturning<TReturn>()
+{
+    public Expression<Func<T1, TReturn>> Wrap<T1, T2>(
+        Expression<Func<T1, T2>> wrapped,
+        Expression<Func<
+            Func<T1, T2>,
+            Expression<Func<T1, TReturn>>
+        >> wrapper)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Expression<Func<T1, TReturn>> Wrap0<T1, T2, T3, T4>(
+        Expression<Func<T1, T2>> wrapped1,
+        Expression<Func<T3, T3>> wrapped2,
+        Expression<Func<
+            Func<T1, T2>,
+            Func<T3, T4>,
+            Expression<Func<T1, TReturn>>
+        >> wrapper)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Expression<FRet> Wrap1<F1, F2, FRet>(
+        Expression<F1> wrapped1,
+        Expression<F2> wrapped2,
+        Expression<Func<
+            F1,
+            F2,
+            Expression<FRet>
+        >> wrapper)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Expression<TReturn> WrapSimple<F1>(
+        Expression<F1> wrapped,
+        Func<F1, TReturn> wrapper)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Expression<TReturn> WrapSimple<F1, F2>(
+        Expression<F1> wrapped1,
+        Expression<F2> wrapped2,
+        Func<F1, F2, TReturn> wrapper)
+    {
+        throw new NotImplementedException();
+    }
 }
