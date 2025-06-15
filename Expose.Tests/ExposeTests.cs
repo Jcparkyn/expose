ae99d8379e5a6677b1fc4c7a5c8aacc71e3b7a1e
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2025-06-15 16:24:33 +1000

    Clean up unused Wrap methods

--- Expose.Tests/ExposeTests.cs
+++ Expose.Tests/ExposeTests.cs
@@ -20,42 +20,6 @@ public class ExposeTests
     }
 
     [Fact]
-    public void Wrap()
-    {
-        Expression<Func<int, int>> addOne = x => x + 1;
-        //var wrapped = Expose.Wrap(addOne, addOne =>
-        //    Expose.Func((int x) => addOne(x) == 3)
-        //);
-        var wrapped = Expose.Wrap<Func<int, int>, Func<int, bool>>(
-            addOne,
-            addOne => (int x) => addOne(x) == 3
-        );
-        var output1 = wrapped.Compile().Invoke(1);
-        output1.Should().BeFalse();
-        var output2 = wrapped.Compile().Invoke(2);
-        output2.Should().BeTrue();
-    }
-
-    [Fact]
-    public void Wrap2()
-    {
-        Expression<Func<int, bool>> isNegative = x => x < 0;
-        Expression<Func<int, int>> mod2 = x => x % 2;
-
-        var wrapped = Expose.Wrap(
-            isNegative, mod2,
-            (isNegative, mod2) => Expose.Func(
-                (int x) => isNegative(x) || mod2(x) == 1
-            )
-        );
-
-        var compiled = wrapped.Compile();
-        compiled.Invoke(-1).Should().BeTrue();
-        compiled.Invoke(0).Should().BeFalse();
-        compiled.Invoke(1).Should().BeTrue();
-    }
-
-    [Fact]
     public void Compose()
     {
         Expression<Func<int, bool>> isNegative = x => x < 0;
--- Expose/Expose.cs
+++ Expose/Expose.cs
@@ -22,35 +22,6 @@ public static class Expose
         return Expression.Lambda<Func<T1, T3>>(replacedBody, firstParameter);
     }
 
-    public static Expression<FReturn> Wrap<F1, FReturn>(
-        Expression<F1> wrapped,
-        Expression<Func<F1, FReturn>> wrapper)
-        where F1 : Delegate
-        where FReturn : Delegate
-    {
-        // Extract the parameter from the wrapper, which is the wrapped expression
-        var wrappedParameter = wrapper.Parameters[0];
-
-        // Extract the body from the wrapper, which is a func use
-        var wrapperBody = wrapper.Body;
-
-        // Substitute the wrapped expression into the wrapper body
-        var replacedBody = new ReplaceParameterVisitor(wrappedParameter, wrapped).Visit(wrapperBody) as LambdaExpression;
-
-        // Finally, create a new lambda with the body of the replaced wrapper
-        var finalLambda = Expression.Lambda<FReturn>(replacedBody.Body, wrapped.Parameters);
-
-        return finalLambda;
-    }
-
-    public static Expression<TReturn> Wrap<F1, F2, TReturn>(
-        Expression<F1> wrapped1,
-        Expression<F2> wrapped2,
-        Expression<Func<F1, F2, TReturn>> wrapper)
-    {
-        throw new NotImplementedException();
-    }
-
     public static Expression<Func<T1, T2>> Compose<T1, T2>(Expression<Func<T1, T2>> value)
     {
         var visitor = new CallMethodReplacer();
@@ -81,74 +52,13 @@ public static class Expose
         }
     }
 
-    private class ReplaceParameterVisitor : ExpressionVisitor
+    private class ReplaceParameterVisitor(ParameterExpression parameter, Expression replacement) : ExpressionVisitor
     {
-        private readonly ParameterExpression _parameter;
-        private readonly Expression _replacement;
-
-        public ReplaceParameterVisitor(ParameterExpression parameter, Expression replacement)
-        {
-            _parameter = parameter;
-            _replacement = replacement;
-        }
-
         protected override Expression VisitParameter(ParameterExpression node)
         {
-            if (node == _parameter)
-                return _replacement;
+            if (node == parameter)
+                return replacement;
             return base.VisitParameter(node);
         }
-    }
-}
-
-public record struct ExposeReturning<TReturn>()
-{
-    public Expression<Func<T1, TReturn>> Wrap<T1, T2>(
-        Expression<Func<T1, T2>> wrapped,
-        Expression<Func<
-            Func<T1, T2>,
-            Expression<Func<T1, TReturn>>
-        >> wrapper)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Expression<Func<T1, TReturn>> Wrap0<T1, T2, T3, T4>(
-        Expression<Func<T1, T2>> wrapped1,
-        Expression<Func<T3, T3>> wrapped2,
-        Expression<Func<
-            Func<T1, T2>,
-            Func<T3, T4>,
-            Expression<Func<T1, TReturn>>
-        >> wrapper)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Expression<FRet> Wrap1<F1, F2, FRet>(
-        Expression<F1> wrapped1,
-        Expression<F2> wrapped2,
-        Expression<Func<
-            F1,
-            F2,
-            Expression<FRet>
-        >> wrapper)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Expression<TReturn> WrapSimple<F1>(
-        Expression<F1> wrapped,
-        Func<F1, TReturn> wrapper)
-    {
-        throw new NotImplementedException();
-    }
-
-    public Expression<TReturn> WrapSimple<F1, F2>(
-        Expression<F1> wrapped1,
-        Expression<F2> wrapped2,
-        Func<F1, F2, TReturn> wrapper)
-    {
-        throw new NotImplementedException();
     }
 }
