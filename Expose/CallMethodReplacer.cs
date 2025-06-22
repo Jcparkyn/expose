b4f4fb874d0bc3aeb750d2721c04abc3023d0f64
Jcparkyn <51850908+Jcparkyn@users.noreply.github.com>
2025-06-22 16:04:06 +1000

    Separate class for extension methods

---
+++ Expose/CallMethodReplacer.cs
@@ -0,0 +1,48 @@
+﻿namespace Expose;
+
+using System.Linq.Expressions;
+using System.Reflection;
+
+internal sealed class CallMethodReplacer : ExpressionVisitor
+{
+    protected override Expression VisitMethodCall(MethodCallExpression node)
+    {
+        if (node.Method.Name == nameof(ExtensionMethods.Call) &&
+            node.Method.DeclaringType == typeof(ExtensionMethods))
+        {
+            var callee = node.Arguments[0];
+            var argument = node.Arguments[1];
+
+            // Replace the Call method call with an invocation of the callee
+            return Expression.Invoke(SimplifyCallee(callee), argument);
+        }
+
+        return base.VisitMethodCall(node);
+    }
+
+    protected override Expression VisitLambda<T>(Expression<T> node)
+    {
+        return base.VisitLambda(node);
+    }
+
+    /// <summary>
+    /// Simplifies the callee expression if it's a reference to a constant field or property.
+    /// </summary>
+    private static Expression SimplifyCallee(Expression callee)
+    {
+        if (callee is MemberExpression
+            {
+                Expression: ConstantExpression { Value: { } constant },
+                Member: var member
+            })
+        {
+            return member switch
+            {
+                FieldInfo fi => Expression.Constant(fi.GetValue(constant), callee.Type),
+                PropertyInfo pi => Expression.Constant(pi.GetValue(constant), callee.Type),
+                _ => callee
+            };
+        }
+        return callee;
+    }
+}
---
+++ Expose/ExtensionMethods.cs
@@ -0,0 +1,54 @@
+﻿namespace Expose;
+
+using System;
+using System.Linq.Expressions;
+
+/// <summary>
+/// Extensions methods for composing and exposing <see cref="Expression"/> trees.
+/// </summary>
+public static class ExtensionMethods
+{
+    /// <summary>
+    /// A method to represent a call to another expression, from within an expression tree. This
+    /// should always be used with <see
+    /// cref="ExpressionComposer.SubstituteCalls{TDelegate}(Expression{TDelegate})"/> to replace the
+    /// call with the actual method invocation.
+    /// </summary>
+    /// <returns>
+    /// Never returns, throws an <see cref="InvalidOperationException"/> if it gets invoked
+    /// </returns>
+    /// <exception cref="InvalidOperationException">if invoked</exception>
+    public static TRet Call<TRet>(this Expression<Func<TRet>> expr)
+    {
+        throw new InvalidOperationException("This method should only be used inside expression trees, and never called");
+    }
+
+    /// <inheritdoc cref="Call{TRet}(Expression{Func{TRet}})"/>
+    public static TRet Call<T1, TRet>(this Expression<Func<T1, TRet>> expr, T1 arg)
+    {
+        throw new InvalidOperationException("This method should only be used inside expression trees, and never called");
+    }
+
+    /// <inheritdoc cref="Call{TRet}(Expression{Func{TRet}})"/>
+    public static TRet Call<T1, T2, TRet>(this Expression<Func<T1, T2, TRet>> expr, T1 arg1, T2 arg2)
+    {
+        throw new InvalidOperationException("This method should only be used inside expression trees, and never called");
+    }
+
+    /// <summary>
+    /// Replaces all calls to the <see
+    /// cref="ExtensionMethods.Call{TRet}(Expression{Func{TRet}})">Call(...)</see> method in an <see
+    /// cref="IQueryable{T}"/> with the actual expressions.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    /// <param name="self"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentNullException"></exception>
+    public static IQueryable<T> SubstituteCalls<T>(this IQueryable<T> self)
+    {
+        _ = self ?? throw new ArgumentNullException(nameof(self));
+        var visitor = new CallMethodReplacer();
+        var newExpression = visitor.Visit(self.Expression);
+        return self.Provider.CreateQuery<T>(newExpression);
+    }
+}
